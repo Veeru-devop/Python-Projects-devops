ff5f6d58adda12580fee94a5484306042dcff3d2
veer pratap singh chouhan <veerpratap7773@gmail.com>
2025-02-12 08:57:51 +0530

    upated commit

--- Git-hub-jira/Github-jira-integration/github-jira-integrate.py
+++ Git-hub-jira/Github-jira-integration/github-jira-integrate.py
@@ -10,9 +10,9 @@ def Jira_integration():
 
     url = "https://veerpratap7773.atlassian.net/rest/api/3/issue"
 
-    API_TOKEN = "ATATT3xFfGF0essCQ4CcFxvevdmPv5K-bgpHEyjL4VlYr_KQciPBRIszwNJ7lWU5etHVObUnj452NmHLeSis7yGC1R5JlDkf5mLJcjsqaJGVHKst-ojqaD-1-d9g8JRcnMmRIq1KOpXY_IAgQfm5pZf8h4gL45C7N_mAAJKhAByOMnUZLn1JCSg=9BFDC33F" 
+    API_TOKEN = "" 
 
-    auth = HTTPBasicAuth("veerpratap7773@gmail.com", API_TOKEN)
+    auth = HTTPBasicAuth("", API_TOKEN)
 
     headers = {
     "Accept": "application/json",
@@ -24,7 +24,7 @@ def Jira_integration():
             "summary": "First JIRA Ticket",
             "project": { "key": "PGA" },
             "issuetype": { "name": "Story" },  
-            "reporter": { "id": "712020:441ca20c-81d2-4edc-8484-55505a4689a1" },  # FIXED: Added required reporter field
+            "reporter": { "id": "" },  # FIXED: Added required reporter field
             "description": {
                 "content": [
                     {
--- Git-hub-jira/listporject.py
+++ Git-hub-jira/listporject.py
@@ -4,9 +4,9 @@ import json
 
 url = "https://veerpratap7773.atlassian.net/rest/api/3/project"
 
-API_TOKEN = "ATATT3xFfGF0essCQ4CcFxvevdmPv5K-bgpHEyjL4VlYr_KQciPBRIszwNJ7lWU5etHVObUnj452NmHLeSis7yGC1R5JlDkf5mLJcjsqaJGVHKst-ojqaD-1-d9g8JRcnMmRIq1KOpXY_IAgQfm5pZf8h4gL45C7N_mAAJKhAByOMnUZLn1JCSg=9BFDC33F" 
+API_TOKEN = "" 
 
-auth = HTTPBasicAuth("veerpratap7773@gmail.com", API_TOKEN)
+auth = HTTPBasicAuth("", API_TOKEN)
 
 headers = {
   "Accept": "application/json"
